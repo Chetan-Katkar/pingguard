8de0afab1652b0382fcf93e0068d8c67509f4a58
Chetan-Katkar <chetankatkar0307@gmail.com>
2026-08-02 17:43:17 +0530

    Project Setup, Docker PostgreSQL, and Initial Config

---
+++ src/main/java/com/pingguard/PingGuardApplication.java
@@ -0,0 +1,13 @@
+package com.pingguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pingguard/PingGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
